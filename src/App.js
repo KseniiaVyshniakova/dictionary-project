6ac527a22b219939306eb63fa6cc4dff3288df3b
KseniiaVyshniakova <87467695+KseniiaVyshniakova@users.noreply.github.com>
2022-08-20 01:17:48 +0300

    added link on GitHub

--- src/App.js
+++ src/App.js
@@ -6,14 +6,21 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <div className="App-header d-flex justify-content-center">
-        <img src={bookmarks} className="App-logo img-fluid" alt="logo" />       
-      <h1>Dictionary</h1>
-      </div>
-      <main>
-        <Dictionary />
-      </main>
-      <footer className="App-footer">Coded by Kseniia Vyshniakova</footer>
+        <div className="App-header d-flex justify-content-center">
+          <img src={bookmarks} className="App-logo img-fluid" alt="logo" />
+          <h1>Dictionary</h1>
+        </div>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          Coded by Kseniia Vyshniakova {" "}
+          <a href="https://github.com/KseniiaVyshniakova/dictionary-project"
+          target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
       </div>
     </div>
   );
